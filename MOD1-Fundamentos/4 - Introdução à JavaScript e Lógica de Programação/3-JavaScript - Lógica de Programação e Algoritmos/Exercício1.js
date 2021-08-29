792630f576167231b2ca90413ef28e2c9dd7c7cf
Renata Domingues <blackbirdfly.r@gmail.com>
2021-08-29 10:28:56 -0300

    Exercicio 4.3

---
+++ MOD1-Fundamentos/4 - Introdução à JavaScript e Lógica de Programação/3-JavaScript - Lógica de Programação e Algoritmos/Exercício1.js
@@ -0,0 +1,14 @@
+
+let n = 5;
+let symbol = '*';
+let inputLine = '';
+
+for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
+  inputLine = inputLine + symbol;
+};
+
+for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
+    console.log(inputLine);
+  };
+
+console.log(inputLine);
